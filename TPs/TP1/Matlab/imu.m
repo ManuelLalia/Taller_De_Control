5afc413f76c90afd63256ff882b6747a19bc99a0
felipeperassi <fperassi@fi.uba.ar>
2025-04-28 14:37:31 -0300

    histogramas

--- TPs/TP1/Matlab/imu.m
+++ TPs/TP1/Matlab/imu.m
@@ -7,3 +7,20 @@ media_angulo = mean(angulo);
 var_angulo = var(angulo);
 
 res_angulo = 3 * sqrt(var_angulo);
+
+% Histograma
+figure;
+histogram(angulo, 50, 'Normalization', 'pdf'); % 20 bins, normalizado como densidad
+hold on;
+
+% Superponemos la campana de Gauss teórica
+x_values = linspace(min(angulo), max(angulo), 100);
+gauss_fit = normpdf(x_values, media_angulo, sqrt(var_angulo));
+plot(x_values, gauss_fit, 'r-', 'LineWidth', 2);
+
+title('Histograma del ángulo medido + Ajuste Gaussiano');
+xlabel('Ángulo (°)');
+ylabel('Densidad de probabilidad');
+legend('Histograma', 'Ajuste Gaussiano');
+grid on;
+hold off;
--- TPs/TP1/Matlab/sensor_digital.m
+++ TPs/TP1/Matlab/sensor_digital.m
@@ -11,3 +11,20 @@ var_tiempo = var(tiempo);
 
 res_dist = 3 * sqrt(var_dist);
 res_tiempo = 3 * sqrt(var_tiempo);
+
+% Histograma
+figure;
+histogram(distancia, 6, 'Normalization', 'pdf'); % 20 bins, normalizado como densidad
+hold on;
+
+% Superponemos la campana de Gauss teórica
+x_values = linspace(min(distancia), max(distancia), 100);
+gauss_fit = normpdf(x_values, media_dist, sqrt(var_dist));
+plot(x_values, gauss_fit, 'r-', 'LineWidth', 2);
+
+title('Histograma de distancia medida + Ajuste Gaussiano');
+xlabel('Distancia (cm)');
+ylabel('Densidad de probabilidad');
+legend('Histograma', 'Ajuste Gaussiano');
+grid on;
+hold off;
