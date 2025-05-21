9e1854d91f9f1e32f720c557425b085193f041f4
ManuelLalia <manulalia01@gmail.com>
2025-05-20 22:08:54 -0300

    Practica 7

--- Practicas/Practica_7/Observador/Observador.ino
+++ Practicas/Practica_7/Observador/Observador.ino
@@ -2,13 +2,6 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
-#include <NewPing.h> // Libreria para la comunicación con el sensor de ultrasonido
-
-#define TRIGGER_PIN  6    // Pin de arduino conectado al pin trigger del sensor de ultrasonido.
-#define ECHO_PIN     7    // Pin de arduino conectado al pin echo del sensor de ultrasonido.
-#define MAX_DISTANCE 100  // Distancia máxima que queremos medir (en cm). El sensor tiene un límite de 400-500cm.
-
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // Se inicializa el sensor con los parametros definidos.
 
 Adafruit_MPU6050 mpu; // Se define el objeto de la IMU
 
@@ -22,9 +15,6 @@ Servo myservo;  // create Servo object to control a servo
 #define ALPHA 0.08
 #define CORRECCION_SERVO 85
 #define CORRECCION_IMU 2.2
-#define PUNTO_0_BARRA 16
-
-#define Ts 0.02
 
 void setup() {
   Serial.begin(115200);
@@ -64,66 +54,59 @@ void setup() {
 void loop() {
   startTime = micros();
 
-  static float error = 0;
-  static float error_ant = 0;
-  static float salida = 0;
-  // static float salida_ant = 0;
-  static float angulo_servo = 0;
-  // static float angulo_servo_ant = 0;
+  float u = cuadrada(0, 30);
 
-  float ref = 0;
+  float A [2][2] = { {1 ,0.02}, {-0.3474, -3.038} } ;
+  float L[2] = {-2.06, 465.69};
+  float B[2] = {0, 1.4676} ;
+  
+  float medicion[2] = {0, 0};
+  medir_angulo(medicion);
+  
+  float angulo = medicion[0] + CORRECCION_IMU;
+  float velocidad = medicion[1];
 
-  salida = medir_distancia();
-  error = ref - salida;
+  static float angulo_est_ant = 0; 
+  static float velocidad_est_ant = 0;
 
-  static float Kp = 3;
-  static float Kd = 0;
-  static float Ki = 0.75;
+  float angulo_est = A[0][0] * angulo_est_ant + A[0][1] * velocidad_est_ant + L[0] * (angulo - angulo_est_ant) + B[0] * u;
+  float velocidad_est = A[1][0] * angulo_est_ant + A[1][1] * velocidad_est_ant + L[1] * (angulo - angulo_est_ant) + B[1] * u;
 
-  static float Ik_ant = 0; 
-  float Ik = Ik_ant + Ts/2 * (error + error_ant);
+  angulo_est_ant = angulo_est;
+  velocidad_est_ant = velocidad_est;
 
-  static float Dk_ant = 0; 
-  float Dk = 2 * (error - error_ant)/Ts - Dk_ant;
-  // Serial.println(Dk);
-  // delay(10000);
-  // if(Dk > 10)
-  //   Dk = 10;
-  // else if(Dk < -10)
-  //   Dk = -10;
+  matlab_send(u, angulo, velocidad, angulo_est, velocidad_est);
+  // theta_g = theta_(mejor) + g_x * delta_t (0.02)
+  // theta_a = f(a_z, a_y) atan2
 
-  angulo_servo = Kp * error + Ki * Ik + Kd * Dk;
-  comandarServo(angulo_servo);
-
-  error_ant = error;
-  Ik_ant = Ik;
-  Dk_ant = Dk;
-
-  // matlab_send(angulo_servo, salida, error, Ik, Dk);
-  matlab_send(Ik, Dk, error);
-  // Serial.println(error);
+  // theta_(mejor) = alpha * theta_a + (1-alpha) * theta_g
 
   endTime = micros();
   
   delay(20 - (endTime-startTime)/1000.0);
 }
 
-float medir_distancia(){
-  return sonar.ping()/(2*29.287) - PUNTO_0_BARRA;
-}
-
-void comandarServo(float angulo){
-  if(angulo < -30){
-    angulo = -30;
-  } else if (angulo > 30){
-    angulo = 30;
+float cuadrada(float inicial, float final){
+  static int angulo_servo = 0;
+  static int contador = 0;
+  if(contador == 0){
+    angulo_servo = inicial;
+    myservo.write(angulo_servo + CORRECCION_SERVO);
   }
   
-  myservo.write(angulo + CORRECCION_SERVO);
-  return;
+  if(contador==200){
+    angulo_servo = final;
+    myservo.write(angulo_servo + CORRECCION_SERVO);
+  }
+
+  contador++;
+  if(contador==400)
+    contador = 0;
+  
+  return angulo_servo;
 }
 
-float medir_angulo(){
+void medir_angulo(float *med){
   sensors_event_t a, g, temp;   // Defino las variables para leer los sensores
   mpu.getEvent(&a, &g, &temp);  // Leo los sensores. ¡¡ El valor de la velocidad angular está en radianes por segundo !!
 
@@ -137,11 +120,11 @@ float medir_angulo(){
   float theta_g_best = theta_best + (g.gyro.x * 0.02) * 180/PI;
   theta_best = ALPHA * theta_a + (1-ALPHA) * theta_g_best;
 
-  return theta_best + CORRECCION_IMU;
+  med[0] = theta_best;
+  med[1] = g.gyro.x * 180/PI;
 }
 
-void matlab_send(float dato1, float dato2, float dato3)//, float dato4, float dato5)
-{
+void matlab_send(float dato1, float dato2, float dato3, float dato4, float dato5){
   // Encabezado que marca el comienzo de los datos
   Serial.write("abcd");
 
@@ -151,8 +134,8 @@ void matlab_send(float dato1, float dato2, float dato3)//, float dato4, float da
   Serial.write(b,4);
   b = (byte *) &dato3;
   Serial.write(b,4);
-  // b = (byte *) &dato4;
-  // Serial.write(b,4);
-  // b = (byte *) &dato5;
-  // Serial.write(b,4);
+  b = (byte *) &dato4;
+  Serial.write(b,4);
+  b = (byte *) &dato5;
+  Serial.write(b,4);
 }
